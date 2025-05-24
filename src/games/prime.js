e861a9cbf5db8bc06d6d8f5cf6509b3419e9d2e0
Mikhail Vyachin <mikhailvyachin2004@gmail.com>
2025-05-24 09:17:27 +0300

    fix description

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,7 +1,7 @@
 import getRandomNumber from '../randomNumber.js';
 import gameLogic from '../index.js';
 
-const description = 'Answer "yes" if given  number is prime. Otherwise answer "no".';
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
   if (num <= 1) return false;
